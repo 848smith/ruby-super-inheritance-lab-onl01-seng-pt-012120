55ae4faa810c63d39e343da75cdc3e825ac279fd
Andrew Smith <848smith@gmail.com>
2020-02-29 23:59:35 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,24 +5,13 @@ class ChattyStudent < Student
       puts "How are you doing today? I'm okay, but I'm kind of tied. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   
+  x = 0
+  
   def raise_hand
+    while x < 10
     super
       "Pick me!"
-    super
-      "Pick me!"
-      super
-      "Pick me!"
-      super
-      "Pick me!"
-      super
-      "Pick me!"
-      super
-      "Pick me!"
-      super
-      "Pick me!"
-      super
-      "Pick me!"
-      super
-      "Pick me!"
+      x += 1
+    end
   end
 end
